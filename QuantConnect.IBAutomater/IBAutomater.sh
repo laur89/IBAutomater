13e94b6c79bd030bc5b1f8a13c2b29440f5912a9
laur <laur.aliste@gmail.com>
2025-02-02 01:33:53 +0100

    IBAutomater.sh: do not kill IDEs
    - replace blanket 'pkill java' with filtering logic that excludes java
      processes backing select IDEs

--- QuantConnect.IBAutomater/IBAutomater.sh
+++ QuantConnect.IBAutomater/IBAutomater.sh
@@ -5,7 +5,12 @@ export JAVA_TOOL_OPTIONS=$2
 ps -AFH
 
 pkill -9 Xvfb
-pkill -9 java
+#pkill -9 java
+
+# note --no-run-if-empty is a GNU extension
+ps --no-headers -eo 'pid,comm,command' \
+    | awk '{ if ($2 == "java" && $3 !~ /\/(Rider|PyCharm(CE)?)20[0-9]+\.[0-9]+/) {print $1} }' \
+    | xargs --no-run-if-empty -- kill -9
 
 sleep 5
 
